865da0bf2eb39b8a24556af399897e4aacbea77b
Gergo06 <58563706+Gergo06-py@users.noreply.github.com>
2020-08-22 12:40:21 +0200

    Update sliders.py

--- sliders.py
+++ sliders.py
@@ -1,19 +1,21 @@
 from tkinter import *
-from PIL import ImageTk,Image
 
 root = Tk()
 root.title("Sliders")
 root.iconbitmap("C:/Users/borbe/Pictures/code.ico")
 root.geometry("400x400")
+root.resizable(False, False)
+
 
 def slide():
     my_label = Label(root, text=horizontal.get()).pack()
-    root.geometry(str(horizontal.get() + 400) + "x" + str(vertical.get() + 400))
+    root.geometry(str(horizontal.get() * 100 + 400) + "x" + str(vertical.get() * 100 + 400))
 
-vertical = Scale(root, from_=0, to=200, orient=VERTICAL)
+
+vertical = Scale(root, from_=0, to=2, orient=VERTICAL, showvalue=FALSE)
 vertical.pack()
 
-horizontal = Scale(from_=0, to=400, orient=HORIZONTAL)
+horizontal = Scale(from_=0, to=4, orient=HORIZONTAL, showvalue=FALSE)
 horizontal.pack()
 
 my_button = Button(text="resize", command=slide).pack()
